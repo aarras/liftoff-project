a46abb674b7eb816da962d0a72bd4a92b951f394
Adam Arras <adamarras@gmail.com>
2021-04-15 19:57:40 -0500

    Added Spring Boot

---
+++ diary_card_spring/diary_card_spring/src/main/java/edu/launchcode/diary_card_spring/DiaryCardSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.launchcode.diary_card_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryCardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryCardSpringApplication.class, args);
+	}
+
+}
---
+++ diary_card_spring/diary_card_spring/src/test/java/edu/launchcode/diary_card_spring/DiaryCardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.launchcode.diary_card_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryCardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
