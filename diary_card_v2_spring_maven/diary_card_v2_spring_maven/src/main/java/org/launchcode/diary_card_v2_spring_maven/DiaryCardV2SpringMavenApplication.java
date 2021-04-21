c95d7d7e4bd005ac70c1695232dcd4dafe57d60a
Adam Arras <adamarras@gmail.com>
2021-04-20 21:26:02 -0500

    Setup complete

---
+++ diary_card_v2_spring_maven/diary_card_v2_spring_maven/src/main/java/org/launchcode/diary_card_v2_spring_maven/DiaryCardV2SpringMavenApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.diary_card_v2_spring_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryCardV2SpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryCardV2SpringMavenApplication.class, args);
+	}
+
+}
---
+++ diary_card_v2_spring_maven/diary_card_v2_spring_maven/src/test/java/org/launchcode/diary_card_v2_spring_maven/DiaryCardV2SpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.diary_card_v2_spring_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryCardV2SpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
