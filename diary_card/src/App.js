5e4a82a57d3002dfbb42c6901997bcbb57769c46
Adam Arras <adamarras@gmail.com>
2021-04-20 21:31:03 -0500

    deleting unwanted files from experiments

--- diary_card/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import './App.css'
-import { Routes } from './Router'
-
-export default Routes
--- diary_card/src/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NavBar } from './NavBar'
-
-export default ({ children }) => (
-  <>
-    <NavBar color={'secondary'} />
-    {children}
-  </>
-)
